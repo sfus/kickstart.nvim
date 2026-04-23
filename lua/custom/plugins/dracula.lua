174f9efb8ad02ce33a851877b486ec7a27a588f1
sfus <sfus.dev@gmail.com>
2026-04-23 17:18:13 +0900

    Delete Dracula theme

--- lua/custom/plugins/dracula.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'Mofiqul/dracula.nvim',
-    priority = 999, -- lower than tokyonight (1000) to load after and override colorscheme
-    config = function()
-      vim.cmd.colorscheme 'dracula'
-    end,
-  },
-}
