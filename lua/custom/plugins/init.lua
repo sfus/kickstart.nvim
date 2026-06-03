95b801c74e2f1f00c0ac63c424c7b52d97a431fa
sfus <sfus.dev@gmail.com>
2026-06-03 14:25:23 +0900

    Add Q keybind

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -20,6 +20,7 @@ vim.opt.wrapscan = false
 vim.keymap.set('n', '<leader>Q', '<cmd>qa<CR>', { silent = true, desc = 'Quit ALL' })
 vim.keymap.set('n', '<M-Q>', 'q', { desc = 'Record macro' })
 vim.keymap.set('n', 'q', '<nop>')
+vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('n', ';', ':')
 
